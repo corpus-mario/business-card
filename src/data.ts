49c223fbeedd07e35c0792e2b43b9032cca73966
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2025-04-11 10:47:34 +0800

    add dummy data

---
+++ src/data.ts
@@ -0,0 +1,33 @@
+export default {
+  name: "Marc Corpus",
+  email: "mcorpus.dev@gmail.com",
+  occupation: "React Native Engineer",
+  social: {
+    github: "https://github.com/corpus-mario",
+    linkedin: "https://www.linkedin.com",
+    website: "https://www.google.com",
+  },
+  bio: "Hey! I'm a React Native engineer who loves building smooth, fast, and user-friendly mobile apps. I'm into clean code, great design, and always learning something new in the mobile dev world.",
+  projects: [
+    {
+      name: "Domino's Pizza | Ordering",
+      image: require("@assets/images/project_1.jpeg"),
+    },
+    {
+      name: "Travel Bee | Travelling",
+      image: require("@assets/images/project_2.jpeg"),
+    },
+    {
+      name: "Food Chef | Food Delivery",
+      image: require("@assets/images/project_3.jpeg"),
+    },
+    {
+      name: "Lushly | Beauty Service",
+      image: require("@assets/images/project_4.jpeg"),
+    },
+    {
+      name: "Cars Centro | Buy & Sell",
+      image: require("@assets/images/project_5.jpeg"),
+    },
+  ],
+};
